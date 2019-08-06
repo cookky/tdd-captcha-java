b922184ba3650a1b165ebc98f09f352449239957
cookky <cookky.developer@gmail.com>
2019-08-06 17:10:03 +0700

    test fail

--- src/test/java/com/example/demo/captcha/CaptchaTests.java
+++ src/test/java/com/example/demo/captcha/CaptchaTests.java
@@ -12,7 +12,7 @@ public class CaptchaTests {
     public void OperandPattern_1_ShouldBe_Minus() {
         Captcha captcha = new Captcha(1, 1, 3, 1);
         String operator = captcha.getOperator();
-        assertEquals("tdd", operator);
+        assertEquals("*", operator);
     }
     @Test
     public void OperandPattern_1_ShouldBe_Delete() {
